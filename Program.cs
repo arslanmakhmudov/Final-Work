c806287ed3ecce05b37e42a28296da1f7401ff0c
Arslan <anderfell2003@mail.ru>
2022-10-02 10:52:06 +0500

    Added array1_checker method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿string[] array1 = new string[4] {"456" "8548" "Orlando" "Echo"};
+﻿string[] array1 = new string[4] {"456", "8548", "Orlando","Echo"};
 string[] array2 = new string[array1.Length];
+void array1_checker(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[i] = array1[i];
+        }
+    }
+}
