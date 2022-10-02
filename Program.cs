2c99c906af1a36ccaceb8284f26d7ea44ae76a16
Arslan <anderfell2003@mail.ru>
2022-10-02 10:56:08 +0500

    Added Print Array method

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 string[] array2 = new string[array1.Length];
 void array1_checker(string[] array1, string[] array2)
 {
+    int count = 0; 
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
             array2[i] = array1[i];
+            count ++;
         }
     }
+}
+void print array(string[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        System.Console.WriteLine($" {array2[i]} ");
+    }
+    System.Console.WriteLine();
 }
