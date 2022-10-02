650b23415cead572e84f6efc6ea09bc560c566b6
Arslan <anderfell2003@mail.ru>
2022-10-02 10:43:40 +0500

    Added arrays and README.md

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[4] {"456" "8548" "Orlando" "Echo"};
+string[] array2 = new string[array1.Length];
